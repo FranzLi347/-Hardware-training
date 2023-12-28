b8b2f14a015db26fd5dfb5f101d01ce10708bc98
Franz Li <ingachi@163.com>
2023-12-28 09:36:51 +0800

    feat

--- src/desktop.c
+++ src/desktop.c
@@ -1,14 +1,13 @@
 #include "desktop.h"
 
-
-typedef struct application{
+typedef struct application
+{
     char *name;
     char *path;
     void (*func)(void);
     int x, y;
     int width;
 } Application;
-
 
 void application_test_pop()
 {
@@ -25,15 +24,13 @@ Application applications[] = {
     {"应用5", "../static/application_icon/lock.bmp", &lock},
 };
 
-
 int lock_flag = 0;
 
 void lock()
 {
     printf("lock\n");
     /* 打开设备文件 /dev/ubuntu_lcd */
-    int lcd_fd = open(DEVICE_NAME, O_RDWR); // 模拟器的设备文件
-
+    int lcd_fd = open(DEVICE_NAME, O_RDWR);
     // int lcd_fd = open("/dev/fb0", O_RDWR);      //实物的设备文件
     if (0 > lcd_fd)
     {
@@ -52,9 +49,6 @@ void lock()
     bzero(ptr, 800 * 480 * 4);
 }
 
-
-
-
 void show_start_img()
 {
 
@@ -63,33 +57,81 @@ void show_start_img()
 
 void show_text(char *text, int x, int y, int size, int color)
 {
-    /* 打开设备文件 */
+
     int lcd_fd = open(DEVICE_NAME, O_RDWR);
-    if (0 > lcd_fd)
+
+    if (strcmp(DEVICE_NAME, "/dev/ubuntu_lcd") == 0)
     {
-        perror("ubuntu_lcd open error!");
-        return -1;
+        /* 打开设备文件 */
+        if (0 > lcd_fd)
+        {
+            perror("ubuntu_lcd open error!");
+            return -1;
+        }
+
+        /* 内存映射 */
+        unsigned int *ptr = (unsigned int *)mmap(NULL, 800 * 480 * 4,
+                                                 PROT_READ | PROT_WRITE, MAP_SHARED, lcd_fd, 0);
+        if (NULL == ptr)
+        {
+            perror("mmap error!");
+            return -1;
+        }
+
+        /* 将需要显示的内容写入到屏幕中 */
+        display_font_to_screen(text, size, color,
+                               x, y, ptr);
+
+        close(lcd_fd);
+
+        return 0;
     }
 
-    /* 内存映射 */
-    unsigned int *ptr = (unsigned int *)mmap(NULL, 800 * 480 * 4,
-                                             PROT_READ | PROT_WRITE, MAP_SHARED, lcd_fd, 0);
-    if (NULL == ptr)
-    {
-        perror("mmap error!");
-        return -1;
-    }
+    // if (strcmp(DEVICE_NAME, "/dev/fb0") == 0)
+    // {
+    //     /* 打开设备文件 */
+    //     if (0 > lcd_fd)
+    //     {
+    //         perror("/dev/fb0 open error!");
+    //         return -1;
+    //     }
 
-    /* 将需要显示的内容写入到屏幕中 */
-    display_font_to_screen(text, size, color,
-                           x, y, ptr);
+    //     /* 内存映射 */
+    //     unsigned int *ptr = (unsigned int *)mmap(NULL, 800 * 480 * 4,
+    //                                              PROT_READ | PROT_WRITE, MAP_SHARED, lcd_fd, 0);
+    //     if (NULL == ptr)
+    //     {
+    //         perror("mmap error!");
+    //         return -1;
+    //     }
 
-    close(lcd_fd);
+    //     /* 打开字体 */
+    //     font *f = fontLoad("/usr/share/fonts/DroidSansFallback.ttf");
+    //     if (NULL == f)
+    //     {
+    //         perror("font open error!");
+    //         return -2;
+    //     }
+    //     /* 调整字体大小 */
+    //     fontSetSize(f, size);
 
-    return 0;
+    //     /* 创建文本框 */
+    //     bitmap *btm_1 = createBitmap(30, 30, 4); // 默认颜色
+
+    //     fontPrint(f, btm_1, 0, 0, text, getColor(0x00, 0x00, 0x00, 0xFF), 30);
+
+    //     /* 将文本框的内容写到屏幕中 */
+    //     /* 注意：文本框设置的宽度+起始位置不能超过屏幕的分辨率范围 */
+    //     show_font_to_lcd(ptr, x, y, btm_1);
+
+    //     /* 关闭字体 */
+    //     destroyBitmap(btm_1);
+    //     fontUnload(f);
+    // }
 }
 
-void clear(){
+void clear()
+{
     /* 打开设备文件 */
     int lcd_fd = open(DEVICE_NAME, O_RDWR);
     if (0 > lcd_fd)
@@ -111,7 +153,7 @@ void clear(){
 }
 
 void render_desktop_application()
-{   
+{
     clear();
     show_bmp("../static/desktop_img.bmp", 0, 0, 1);
     show_text("欢迎使用", 260, 130, 60, 0xF5F5F5);
@@ -130,12 +172,12 @@ void listen_touch()
 
     int touch_fd = open(DEVICE_NAME, O_RDWR); // 模拟屏
 
-    // int touch_fd = open("/dev/input/event0", O_RDWR);  //实物的
     if (0 > touch_fd)
     {
         perror("event open error");
         return -1;
     }
+
     struct input_event ts;
 
     int ts_x = -1, ts_y = -1;
@@ -172,15 +214,14 @@ void listen_touch()
         {
             if (ts_x > applications[i].x && ts_x < applications[i].x + 100 && ts_y > applications[i].y && ts_y < applications[i].y + 100)
             {
-                printf("x = %d, y = %d\n", ts_x, ts_y);
                 applications[i].func();
+                render_desktop_application();
             }
         }
     }
     else
     {
         lock_flag = 0;
-        // show_desktop();
         render_desktop_application();
     }
     /* 收尾操作 */
